faa18da2224db62474dda290f1e602331fed0569
TassiloKloos <Tassilo.Kloos@gmx.de>
2015-11-17 14:08:00 +0100

    First Upload
    This is the first test to see if everything works fine

---
+++ observer.go
@@ -0,0 +1,31 @@
+package observer
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func openShell(list string) bool {
+	cmd := exec.Command(list)
+	stdout, err := cmd.Output()
+	if err != nil {
+		//fmt.Println(err)
+		return false
+	}
+	fmt.Println(string(stdout))
+	return true
+}
+
+func enterCommand(command string, location string) bool {
+	cmd := exec.Command(command, location)
+
+	cmd.Run()
+	stdout, err := cmd.Output()
+	if !strings.Contains(err.Error(), "started") {
+		//fmt.Println(err)
+		return false
+	}
+	fmt.Println(string(stdout))
+	return true
+}
---
+++ observer_test.go
@@ -0,0 +1,31 @@
+package observer
+
+import "testing"
+
+func TestOpenShell(t *testing.T) {
+	var solve bool = openShell("tasklist")
+	if solve != true {
+		t.Error("Fehler beim Öffnen der Shell")
+	}
+}
+
+func TestOpenShellWrong(t *testing.T) {
+	var solve bool = openShell("x")
+	if solve != false {
+		t.Error("Öffnen der falschen Shell möglich")
+	}
+}
+
+func TestEnterCommand(t *testing.T) {
+	var solve bool = enterCommand("explorer.exe", "C:\\Go\\src\\_Programme\\Project")
+	if solve != true {
+		t.Error("Fehler beim Eingeben von Commands")
+	}
+}
+
+func TestEnterWrongCommand(t *testing.T) {
+	var solve bool = enterCommand("x", "y")
+	if solve != false {
+		t.Error("Eingeben von falschen Commands möglich")
+	}
+}
