b85f651265df2782c791c2a8e715e312497f7f52
Carsten <splincore@web.de>
2015-12-01 15:07:33 +0100

    2 buttons
    added buttons to start available apps

--- observer.go
+++ observer.go
@@ -5,8 +5,21 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"strconv"
 	"strings"
 )
+
+var availableApps []string
+var availableAppsButtonHTML string
+var runningProcesses []*exec.Cmd
+
+func readXML() { //später hier das XML auslesen
+	availableApps = append(availableApps, "C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
+	availableApps = append(availableApps, "C:/WINDOWS/system32/notepad.exe")
+	a := "<form action='/procStart/?procStartID=0' method='post'><input type='submit' value='Firefox Starten'></form>"
+	b := "<form action='/procStart/?procStartID=1' method='post'><input type='submit' value='Notepad Starten'></form>"
+	availableAppsButtonHTML = a + b
+}
 
 func openShell(list string) bool {
 	cmd := exec.Command(list)
@@ -32,6 +45,29 @@ func enterCommand(command string, location string) bool {
 	return true
 }
 
+func procStartHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	procStart, _ := strconv.ParseInt(q.Get("procStartID"), 0, 32)
+
+	cmd := exec.Command(availableApps[procStart])
+	cmd.Run()
+	runningProcesses = append(runningProcesses, cmd)
+
+	//evtl hier Fehler abfangen
+	//stdout, err := cmd.Output()
+	//if err != nil {
+	//	println(err.Error())
+	//}
+	//print(string(stdout))
+
+	responseString := "<html><head><title></title></head><body>" +
+		"<form action='/proc/?procNr=ID0' method='post'><input type='submit' value='Prozess 0'></form>" +
+		"<form action='/proc/?procNr=ID1' method='post'><input type='submit' value='Prozess 1'></form>" +
+		availableAppsButtonHTML +
+		"</body></html>"
+	w.Write([]byte(responseString))
+}
+
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	name := q.Get("name")
@@ -41,9 +77,11 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	responseString := "<html><head><title></title></head><body>" +
 		"<form action='/proc/?procNr=ID0' method='post'><input type='submit' value='Prozess 0'></form>" +
 		"<form action='/proc/?procNr=ID1' method='post'><input type='submit' value='Prozess 1'></form>" +
+		availableAppsButtonHTML +
 		"</body></html>"
 	w.Write([]byte(responseString))
 }
+
 func procHandler(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	procNr := q.Get("procNr")
@@ -54,8 +92,13 @@ func procHandler(w http.ResponseWriter, r *http.Request) {
 	responseString := "<html><head></head><body>" + procNr + "</body></html>"
 	w.Write([]byte(responseString))
 }
+
 func main() {
+	readXML()
+
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/proc/", procHandler)
+	http.HandleFunc("/procStart/", procStartHandler)
 	log.Fatalln(http.ListenAndServe(":8080", nil))
+
 }
