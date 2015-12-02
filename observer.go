50efef512a41467e86b46a54279e0cbf5260190e
Carsten <splincore@web.de>
2015-12-02 12:51:49 +0100

    some overhaul
    still need to check if process still running

--- observer.go
+++ observer.go
@@ -4,81 +4,87 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"strconv"
-	"strings"
 )
 
 var availableApps []string
 var availableAppsButtonHTML string
 var runningProcesses []*exec.Cmd
+var stopProcessButtons string
+
+const responseStringFirstLine string = "<html><head><title></title></head><body>"
+const responseStringLastLine string = "</body></html>"
 
 func readXML() { //später hier das XML auslesen
 	availableApps = append(availableApps, "C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
 	availableApps = append(availableApps, "C:/WINDOWS/system32/notepad.exe")
+	availableApps = append(availableApps, "D:\\Uni\\5. Semester\\Programmieren 2\\test.bat")
 	a := "<form action='/procStart/?procStartID=0' method='post'><input type='submit' value='Firefox Starten'></form>"
 	b := "<form action='/procStart/?procStartID=1' method='post'><input type='submit' value='Notepad Starten'></form>"
-	availableAppsButtonHTML = a + b
+	c := "<form action='/procStart/?procStartID=2' method='post'><input type='submit' value='TestApp Starten'></form>"
+	availableAppsButtonHTML = a + b + c
 }
 
-func openShell(list string) bool {
-	cmd := exec.Command(list)
-	stdout, err := cmd.Output()
-	if err != nil {
-		//fmt.Println(err)
-		return false
+func createStopButtons() {
+	stopProcessButtons = ""
+	for processNR := range runningProcesses { //überprüfen, ob Prozess noch läuft
+		proc, err := os.FindProcess(runningProcesses[processNR].Process.Pid)
+		fmt.Println(proc.Pid)
+		if err == nil {
+			stopProcessButtons = stopProcessButtons + "<form action='/proc/?procNr=" + strconv.Itoa(processNR) + "' method='post'><input type='submit' value='Prozess " + runningProcesses[processNR].Path + " anhalten'></form>"
+		}
 	}
-	fmt.Println(string(stdout))
-	return true
 }
 
-func enterCommand(command string, location string) bool {
-	cmd := exec.Command(command, location)
+//func openShell(list string) bool {
+//	cmd := exec.Command(list)
+//	stdout, err := cmd.Output()
+//	if err != nil {
+//		//fmt.Println(err)
+//		return false
+//	}
+//	fmt.Println(string(stdout))
+//	return true
+//}
 
-	cmd.Run()
-	stdout, err := cmd.Output()
-	if !strings.Contains(err.Error(), "asdf") {
-		//fmt.Println(err)
-		return false
-	}
-	fmt.Println(string(stdout))
-	return true
-}
+//func enterCommand(command string, location string) bool {
+//	cmd := exec.Command(command, location)
+
+//	cmd.Run()
+//	stdout, err := cmd.Output()
+//	if !strings.Contains(err.Error(), "started") {
+//		//fmt.Println(err)
+//		return false
+//	}
+//	fmt.Println(string(stdout))
+//	return true
+//}
 
 func procStartHandler(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	procStart, _ := strconv.ParseInt(q.Get("procStartID"), 0, 32)
 
 	cmd := exec.Command(availableApps[procStart])
-	cmd.Run()
+
+	//cmd := exec.Command("D:\\Uni\\5. Semester\\Programmieren 2\\test.bat")
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cmd.Process.Pid)
 	runningProcesses = append(runningProcesses, cmd)
 
-	//evtl hier Fehler abfangen
-	//stdout, err := cmd.Output()
-	//if err != nil {
-	//	println(err.Error())
-	//}
-	//print(string(stdout))
-
-	responseString := "<html><head><title></title></head><body>" +
+	cmd.Process.Kill()
+	createStopButtons()
+	responseString := responseStringFirstLine +
+		availableAppsButtonHTML +
 		"<form action='/proc/?procNr=ID0' method='post'><input type='submit' value='Prozess 0'></form>" +
 		"<form action='/proc/?procNr=ID1' method='post'><input type='submit' value='Prozess 1'></form>" +
-		availableAppsButtonHTML +
-		"</body></html>"
-	w.Write([]byte(responseString))
-}
+		stopProcessButtons +
+		responseStringLastLine
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	name := q.Get("name")
-	if name == "" {
-		name = "World"
-	}
-	responseString := "<html><head><title></title></head><body>" +
-		"<form action='/proc/?procNr=ID0' method='post'><input type='submit' value='Prozess 0'></form>" +
-		"<form action='/proc/?procNr=ID1' method='post'><input type='submit' value='Prozess 1'></form>" +
-		availableAppsButtonHTML +
-		"</body></html>"
 	w.Write([]byte(responseString))
 }
 
@@ -89,7 +95,25 @@ func procHandler(w http.ResponseWriter, r *http.Request) {
 		procNr = "notFound"
 	}
 	fmt.Println(procNr)
-	responseString := "<html><head></head><body>" + procNr + "</body></html>"
+	responseString := responseStringFirstLine +
+		availableAppsButtonHTML +
+		"<form action='/proc/?procNr=ID0' method='post'><input type='submit' value='Prozess 0'></form>" +
+		"<form action='/proc/?procNr=ID1' method='post'><input type='submit' value='Prozess 1'></form>" +
+		responseStringLastLine
+	w.Write([]byte(responseString))
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	name := q.Get("name")
+	if name == "" {
+		name = "World"
+	}
+	responseString := responseStringFirstLine +
+		availableAppsButtonHTML +
+		"<form action='/proc/?procNr=ID0' method='post'><input type='submit' value='Prozess 0'></form>" +
+		"<form action='/proc/?procNr=ID1' method='post'><input type='submit' value='Prozess 1'></form>" +
+		responseStringLastLine
 	w.Write([]byte(responseString))
 }
 
